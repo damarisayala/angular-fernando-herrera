cf00e20b3640093a14adddc95d47649b0659fae2
damarisayala <damarisayala02@gmail.com>
2025-10-28 16:38:39 -0400

    ng_for

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, signal } from '@angular/core';
 
+interface Character{
+  id: number;
+  name: string;
+  power: number;
+}
 @Component({
-  imports: [RouterLink, RouterLinkActive ],
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    {id: 1, name: 'One push', power: 9001},
+    {id: 2, name: 'Zonic', power: 1500},
+    {id: 3, name: 'Hero', power: 600},
+  ]);
+
+
 
 }
